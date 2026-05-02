70195e11213d5f98a308c78ffd2ba784014c23d6
dewaayunadya <nadyasetia99@gmail.com>
2026-05-02 23:14:42 +0800

    Multimedia Interaktif Musik Tradisional

---
+++ story_content/triggers.js
@@ -0,0 +1,98 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6RjA9qr3v7o":
+        Script1();
+        break;
+      case "67ZEI51gW93":
+        Script2();
+        break;
+      case "6pi6JEu5E2G":
+        Script3();
+        break;
+      case "6bKHPxgz8Xk":
+        Script4();
+        break;
+      case "5ksuq3SbFKS":
+        Script5();
+        break;
+      case "6W2NTve0try":
+        Script6();
+        break;
+      case "6RvH8ho3s25":
+        Script7();
+        break;
+      case "65zQLGtktxg":
+        Script8();
+        break;
+      case "6p9sK6cQVQM":
+        Script9();
+        break;
+      case "5VWFd6kEAwK":
+        Script10();
+        break;
+      case "5yAN0aghtAE":
+        Script11();
+        break;
+      case "6YWBe3cyFIh":
+        Script12();
+        break;
+      case "6DVNxs1pECU":
+        Script13();
+        break;
+      case "6L8vvS8BTiS":
+        Script14();
+        break;
+      case "5rQIt10slkr":
+        Script15();
+        break;
+      case "6lazFuknDk6":
+        Script16();
+        break;
+      case "6LVlUHPlpyX":
+        Script17();
+        break;
+      case "6GiH49RPbgs":
+        Script18();
+        break;
+      case "6lc4YFok7F5":
+        Script19();
+        break;
+      case "67EywvGVxtS":
+        Script20();
+        break;
+      case "5bqIariBgmp":
+        Script21();
+        break;
+      case "6RePcHMkts6":
+        Script22();
+        break;
+      case "6nP1AVMEA8n":
+        Script23();
+        break;
+      case "6aM7G0R9vF4":
+        Script24();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
